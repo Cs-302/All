1ba9096d837a432ad0fd34fa60e855a3ad019416
ajmul <amulhern@edgewood.edu>
2020-03-02 18:57:02 -0600

    Changes to register.php css

--- register.php
+++ register.php
@@ -125,7 +125,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <title>Sign Up</title>
     <link rel="stylesheet" href="All/Styling.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    //<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
         body {
             font: 14px sans-serif;
